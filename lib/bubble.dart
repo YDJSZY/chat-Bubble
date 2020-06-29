83563458ee7dd4c16940736f27a676d544abf904
YDJSZY <lwwzy1314@163.com>
2020-06-29 21:42:30 +0800

    去掉打印

--- lib/bubble.dart
+++ lib/bubble.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-const _ArrowWidth = 7.0;    //箭头宽度
-const _ArrowHeight = 10.0;  //箭头高度
-const _MinHeight = 32.0;    //最小高度
-const _MinWidth = 50.0;     //最小宽度
+const _ArrowWidth = 7.0; //箭头宽度
+const _ArrowHeight = 10.0; //箭头高度
+const _MinHeight = 32.0; //最小高度
+const _MinWidth = 50.0; //最小宽度
 
 class Bubble extends StatelessWidget {
   final BubbleDirection direction;
@@ -23,7 +23,15 @@ class Bubble extends StatelessWidget {
       {Key key,
       this.direction = BubbleDirection.left,
       this.borderRadius,
-      this.child, this.decoration, this.color, this.padding, this.margin, this.constraints, this.width, this.height, this.alignment})
+      this.child,
+      this.decoration,
+      this.color,
+      this.padding,
+      this.margin,
+      this.constraints,
+      this.width,
+      this.height,
+      this.alignment})
       : _left = direction == BubbleDirection.left ? _ArrowWidth : 0.0,
         _right = direction == BubbleDirection.right ? _ArrowWidth : 0.0,
         super(key: key);
@@ -37,11 +45,13 @@ class Bubble extends StatelessWidget {
         alignment: this.alignment,
         width: this.width,
         height: this.height,
-        constraints: (this.constraints??BoxConstraints()).copyWith(minHeight: _MinHeight,minWidth: _MinWidth),
+        constraints: (this.constraints ?? BoxConstraints())
+            .copyWith(minHeight: _MinHeight, minWidth: _MinWidth),
         margin: this.margin,
         decoration: this.decoration,
         color: this.color,
-        padding: EdgeInsets.fromLTRB(this._left, 0.0, this._right, 0.0).add(this.padding??EdgeInsets.fromLTRB(7.0, 5.0, 7.0, 5.0)),
+        padding: EdgeInsets.fromLTRB(this._left, 0.0, this._right, 0.0)
+            .add(this.padding ?? EdgeInsets.fromLTRB(7.0, 5.0, 7.0, 5.0)),
         child: this.child,
       ),
     );
@@ -60,28 +70,30 @@ class _BubbleClipper extends CustomClipper<Path> {
   Path getClip(Size size) {
     final path = Path();
     final path2 = Path();
-    final centerPoint = (size.height / 2).clamp(_MinHeight/2, _MinHeight/2);
-    print(centerPoint);
+    final centerPoint = (size.height / 2).clamp(_MinHeight / 2, _MinHeight / 2);
     if (this.direction == BubbleDirection.left) {
       //绘制左边三角形
       path.moveTo(0, centerPoint);
-      path.lineTo(_ArrowWidth, centerPoint - _ArrowHeight/2);
-      path.lineTo(_ArrowWidth, centerPoint + _ArrowHeight/2);
+      path.lineTo(_ArrowWidth, centerPoint - _ArrowHeight / 2);
+      path.lineTo(_ArrowWidth, centerPoint + _ArrowHeight / 2);
       path.close();
       //绘制矩形
       path2.addRRect(RRect.fromRectAndRadius(
-          Rect.fromLTWH(_ArrowWidth, 0, (size.width - _ArrowWidth), size.height), this.radius));
+          Rect.fromLTWH(
+              _ArrowWidth, 0, (size.width - _ArrowWidth), size.height),
+          this.radius));
       //合并
       path.addPath(path2, Offset(0, 0));
     } else {
       //绘制右边三角形
       path.moveTo(size.width, centerPoint);
-      path.lineTo(size.width - _ArrowWidth, centerPoint - _ArrowHeight/2);
-      path.lineTo(size.width - _ArrowWidth, centerPoint + _ArrowHeight/2);
+      path.lineTo(size.width - _ArrowWidth, centerPoint - _ArrowHeight / 2);
+      path.lineTo(size.width - _ArrowWidth, centerPoint + _ArrowHeight / 2);
       path.close();
       //绘制矩形
       path2.addRRect(RRect.fromRectAndRadius(
-          Rect.fromLTWH(0, 0, (size.width - _ArrowWidth), size.height), this.radius));
+          Rect.fromLTWH(0, 0, (size.width - _ArrowWidth), size.height),
+          this.radius));
       //合并
       path.addPath(path2, Offset(0, 0));
     }
